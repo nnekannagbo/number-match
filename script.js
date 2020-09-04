d85dea4ddc047d3763f48196b1cb25d20935661b
Nneka Nnagbo <nnekakassandra@gmail.com>
2020-09-04 16:51:48 -0400

    Update js timer

--- script.js
+++ script.js
@@ -1,90 +1,113 @@
 const images = [
     {
-      image_name: 'bananas.jpg',
+      image_name: "bananas.jpg",
       number_of_items: 6,
     },
     {
-      image_name: 'birthday candles.jpg',
+      image_name: "birthday candles.jpg",
       number_of_items: 7,
     },
     {
-      image_name: 'blocks.jpg',
+      image_name: "blocks.jpg",
       number_of_items: 6,
     },
     {
-      image_name: 'brushes.jpg',
+      image_name: "brushes.jpg",
       number_of_items: 7,
     },
     {
-      image_name: 'cakes.jpg',
+      image_name: "cakes.jpg",
       number_of_items: 7,
     },
     {
-      image_name: 'cars.jpg',
+      image_name: "cars.jpg",
       number_of_items: 2,
     },
     {
-      image_name: 'crayons.jpg',
+      image_name: "crayons.jpg",
       number_of_items: 8,
     },
     {
-      image_name: 'cupcakes.jpg',
+      image_name: "cupcakes.jpg",
       number_of_items: 7,
     },
     {
-      image_name: 'deer.jpg',
+      image_name: "deer.jpg",
       number_of_items: 3,
     },
     {
-      image_name: 'donuts.jpg',
+      image_name: "donuts.jpg",
       number_of_items: 6,
     },
     {
-      image_name: 'ducks.jpg',
+      image_name: "ducks.jpg",
       number_of_items: 6,
     },
     {
-      image_name: 'eggs.jpg',
+      image_name: "eggs.jpg",
       number_of_items: 8,
     },
     {
-      image_name: 'elephants.jpg',
+      image_name: "elephants.jpg",
       number_of_items: 7,
     },
     {
-      image_name: 'hot air balloons.jpg',
+      image_name: "hot air balloons.jpg",
       number_of_items: 5,
     },
     {
-      image_name: 'jelly beans.jpg',
+      image_name: "jelly beans.jpg",
       number_of_items: 9,
     },
     {
-      image_name: 'macaroons.jpg',
+      image_name: "macaroons.jpg",
       number_of_items: 7,
     },
     {
-      image_name: 'pencils.jpg',
+      image_name: "pencils.jpg",
       number_of_items: 12,
     },
     {
-      image_name: 'people.jpg',
+      image_name: "people.jpg",
       number_of_items: 6,
     },
     {
-      image_name: 'peppers.jpg',
+      image_name: "peppers.jpg",
       number_of_items: 2,
     },
     {
-      image_name: 'pizza slices.jpg',
+      image_name: "pizza slices.jpg",
       number_of_items: 8,
     },
   ];
-
-const randomNumber = Math.floor(Math.random() * images.length);
-const randomImageName = images[randomNumber].image_name;
-const imageContainer = document.getElementById("imageContainer");
-
-const image = document.createElement("img");
-image.src = `images/${randomImageName}`;
-imageContainer.appendChild(image);
+  
+  const generate = () => {
+    if (images.length === 0) {
+      stopTimer();
+      return;
+    }
+    const randomNumber = Math.floor(Math.random() * images.length);
+    const randomImageName = images[randomNumber].image_name;
+    const imageContainer = document.getElementById("imageContainer");
+    if (imageContainer.hasChildNodes()) {
+      imageContainer.removeChild(imageContainer.firstElementChild);
+    }
+    const image = document.createElement("img");
+    image.src = `images/${randomImageName}`;
+    imageContainer.appendChild(image);
+    images.splice(randomNumber, 1);
+  };
+  
+  let timerRef;
+  const timer = () => {
+    timerRef = setInterval(generate, 500);
+  };
+  
+  const play = () => {
+    generate();
+    timer();
+  };
+  
+  const stopTimer = () => {
+    clearInterval(timerRef);
+  };
